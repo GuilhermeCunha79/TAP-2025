3847a7ebb599ddd63f0e3fb2dfbba83b81d672af
Guilherme Cunha <1201506@isep.ipp.pt>
2025-04-25 17:45:41 +0000

    Update: Clean up on test files

--- src/test/scala/pj/io/FileIOTest.scala
+++ src/test/scala/pj/io/FileIOTest.scala
@@ -7,19 +7,19 @@ import java.io.{File, PrintWriter}
 
 class FileIOTest extends AnyFunSuite:
 
-  val testFilesDir = "files/group/ms01/"
+  val testFilesDir = "src/test/scala/files/test/ms01/"
 
-  test("loadError deve carregar o arquivo emptyMessageAttribute_outError.xml corretamente"):
+  test("loadError with empty message attribute - Should return Right with 'InvalidTime(0)'"):
     val result = FileIO.loadError(testFilesDir + "emptyMessageAttribute_outError.xml")
     assert(result.isRight)
     assert(result.getOrElse("") == "InvalidTime(0)")
 
-  test("loadError deve carregar o arquivo noMessageAttribute_outError.xml corretamente"):
+  test("loadError with missing message attribute - Should return Right with 'InvalidTime(-5)'"):
     val result = FileIO.loadError(testFilesDir + "noMessageAttribute_outError.xml")
     assert(result.isRight)
     assert(result.getOrElse("") == "InvalidTime(-5)")
 
-  test("load should return Right(Elem) when XML file exists and is valid"):
+  test("load with valid XML file - Should return Right(Elem)"):
     val file = File.createTempFile("test", ".xml")
     val writer = new PrintWriter(file)
     writer.write("<root><child>content</child></root>")
@@ -30,13 +30,11 @@ class FileIOTest extends AnyFunSuite:
 
     file.delete()
 
-  test("load should return Left(IOFileProblem) when file does not exist"):
+  test("load with non-existent file - Should return Left(IOFileProblem)"):
     val result = FileIO.load("non_existent_file.xml")
-    result match
-      case Left(_: IOFileProblem) => succeed
-      case _ => fail("Expected IOFileProblem")
+    assert(result.fold(_.toString.contains("IOFileProblem"), _ => false))
 
-  test("loadError returns Right(message) when XML has 'message' attribute"):
+  test("loadError with valid message attribute - Should return Right(message)"):
     val file = File.createTempFile("valid", ".xml")
     val writer = new PrintWriter(file)
     writer.write("""<root message="Something went wrong"/>""")
@@ -47,23 +45,17 @@ class FileIOTest extends AnyFunSuite:
 
     file.delete()
 
-  test("loadError returns Left(IOFileProblem) when XML has no 'message' attribute"):
+  test("loadError with missing message attribute - Should return Left with missing message error"):
     val file = File.createTempFile("invalid", ".xml")
     val writer = new PrintWriter(file)
     writer.write("""<root/>""")
     writer.close()
 
     val result = FileIO.loadError(file.getAbsolutePath)
-    assert(result match
-      case Left(IOFileProblem(msg)) => msg.contains("does not have message")
-      case _ => false
-    )
-
     file.delete()
 
-  test("loadError returns Left(IOFileProblem) when file does not exist"):
+    assert(result.fold(_.toString.contains("does not have message"), _ => false))
+
+  test("loadError with non-existent file - Should return Left(IOFileProblem)"):
     val result = FileIO.loadError("nonexistent.xml")
-    assert(result match
-      case Left(IOFileProblem(_)) => true
-      case _ => false
-    )
+    assert(result.fold(_.toString.contains("IOFileProblem"), _ => false))
