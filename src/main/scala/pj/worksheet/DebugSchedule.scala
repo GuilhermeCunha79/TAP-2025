e37f16124ab078aaea46de2a2d1540836bb418d3
JoseMendes1230195 <1230195@isep.ipp.pt>
2025-06-19 16:08:15 +0100

    Update: Added way to process single or multi files

--- src/main/scala/pj/worksheet/DebugSchedule.scala
+++ src/main/scala/pj/worksheet/DebugSchedule.scala
@@ -7,27 +7,33 @@ import java.io.File
 
 object DebugSchedule:
   def main(args: Array[String]): Unit =
-    val dirPath = "C:\\Users\\zpedr\\OneDrive\\Ambiente de Trabalho\\tap_proj\\tap-pj-ncf-12\\files\\assessment\\ms03"
+    val dirPath = "C:\\Users\\zpedr\\OneDrive\\Ambiente de Trabalho\\tap_proj\\tap-pj-ncf-12\\files\\assessment\\ms03\\validAgenda_65_in.xml"
     val dir = new File(dirPath)
 
-    if dir.exists && dir.isDirectory then
-      val files = dir.listFiles().filter(f => f.getName.startsWith("validAgenda") && f.getName.endsWith("in.xml"))
+    if dir.exists then
+      if dir.isDirectory then
+        val files = dir.listFiles().filter(f => f.getName.startsWith("validAgenda") && f.getName.endsWith("in.xml"))
 
-      if files.isEmpty then
-        println("Nenhum ficheiro válido encontrado.")
-      else
-        files.foreach { file =>
-          println(s"A processar ficheiro: ${file.getName}")
+        if files.isEmpty then
+          println("Nenhum ficheiro válido encontrado.")
+        else
+          files.foreach(processFile)
+      else if dir.isFile then
+        processFile(dir)
+    else
+      println(s"Caminho inexistente: $dirPath")
 
-          FileIO.load(file.getAbsolutePath) match
-            case Right(xml) =>
-              val outputFileName = file.getName.replace("_in", "_out")
-              ScheduleMS03.create(xml,outputFileName) match
-                case Right(s) =>
-                  println(s"Agendamento criado com sucesso para ${file.getName}")
-                case Left(error) =>
-                  println(s"Erro ao criar o agendamento para ${file.getName}: $error")
+  def processFile(file: File): Unit =
+    println(s"A processar ficheiro: ${file.getName}")
 
-            case Left(error) =>
-              println(s"Erro ao carregar o ficheiro ${file.getName}: $error")
-        }
+    FileIO.load(file.getAbsolutePath) match
+      case Right(xml) =>
+        val outputFileName = file.getName.replace("_in", "_out")
+        ScheduleMS03.create(xml, outputFileName) match
+          case Right(s) =>
+            println(s"Agendamento criado com sucesso para ${file.getName}")
+          case Left(error) =>
+            println(s"Erro ao criar o agendamento para ${file.getName}: $error")
+
+      case Left(error) =>
+        println(s"Erro ao carregar o ficheiro ${file.getName}: $error")
